fbaf024a6c273da5dac71fd3be9391d74507e14e
Kostyantyn <one3121man@gmail.com>
2022-07-03 21:12:03 +0300

    Create MainController.java

---
+++ src/main/java/ua/oneman/simple_shop/controllers/MainController.java
@@ -0,0 +1,19 @@
+package ua.oneman.simple_shop.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        return "home";
+    }
+
+}
