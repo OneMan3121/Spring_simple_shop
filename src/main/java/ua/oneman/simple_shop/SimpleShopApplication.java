e68610674aa2b7605c4963441c42319fe1f93040
Kostyantyn <one3121man@gmail.com>
2022-07-03 21:07:57 +0300

    first commit

---
+++ src/main/java/ua/oneman/simple_shop/SimpleShopApplication.java
@@ -0,0 +1,13 @@
+package ua.oneman.simple_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/oneman/simple_shop/SimpleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.oneman.simple_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
